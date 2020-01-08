5e29c61cbd5b12374a62841ed7798f2b1e2ea554
jixing <1544916656@qq.com>
2020-01-08 23:11:05 +0800

    第二次提交，完成后台环境搭建和配置路由

---
+++ serve/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import {Button} from 'antd';
+
+function Login(){
+    return(
+        <div>
+            我是登录界面
+            <Button type="primary">Hello World</Button>
+        </div>
+    )
+}
+
+export default Login
